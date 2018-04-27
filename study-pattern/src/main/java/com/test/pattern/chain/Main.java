4d64367b5ea5b133436c3c8fd9e512adac944351
dranawhite <1458755325@qq.com>
2018-04-27 10:58:58 +0800

    修复JavaDoc

--- study-pattern/src/main/java/com/test/pattern/chain/Main.java
+++ study-pattern/src/main/java/com/test/pattern/chain/Main.java
@@ -6,7 +6,7 @@ import com.dranawhite.api.model.BaseRequest;
  * 职责链模式
  * <pre>
  *     职责链中的每个节点都会遍历到
- *     Manager ——> Director ——> Master
+ *     Manager —— Director —— Master
  * </pre>
  *
  * @author dranawhite 2018/1/2
