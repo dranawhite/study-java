9ea863e9a9b2aa4adc4b8c6be23a98223c7898f5
dranawhite <1458755325@qq.com>
2018-04-27 11:03:21 +0800

    冒泡排序基准测试

--- study-basic/study-algorithm/src/main/java/com/test/algorithm/sort/benchmark/InsertSortBenchmakData.java
+++ study-basic/study-algorithm/src/main/java/com/test/algorithm/sort/benchmark/InsertSortBenchmakData.java
@@ -1,5 +1,6 @@
 package com.test.algorithm.sort.benchmark;
 
+import com.dranawhite.test.jmh.AverageTimeBenchmark;
 import com.test.algorithm.sort.Sort;
 import com.test.algorithm.sort.simple.InsertSort;
 import org.openjdk.jmh.annotations.Benchmark;
@@ -10,7 +11,7 @@ import org.openjdk.jmh.annotations.Benchmark;
  * @author liangyq
  * @version [1.0, 2018/4/26 17:34]
  */
-public class InsertSortBenchmakData  {
+public class InsertSortBenchmakData extends AverageTimeBenchmark {
 
 	@Benchmark
 	public void benchmark() {
