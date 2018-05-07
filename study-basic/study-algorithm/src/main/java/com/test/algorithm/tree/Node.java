2627d98ce43586fb47157c9840ba55017261d208
dranawhite <1458755325@qq.com>
2018-05-07 20:27:32 +0800

    二叉搜索树

---
+++ study-basic/study-algorithm/src/main/java/com/test/algorithm/tree/Node.java
@@ -0,0 +1,13 @@
+package com.test.algorithm.tree;
+
+/**
+ * @author liangyq
+ * @version [1.0, 2018/5/7 17:55]
+ */
+public class Node {
+
+	int key;
+	double data;
+	Node left;
+	Node right;
+}
---
+++ study-basic/study-algorithm/src/main/java/com/test/algorithm/tree/Tree.java
@@ -0,0 +1,16 @@
+package com.test.algorithm.tree;
+
+/**
+ * 树
+ *
+ * @author liangyq
+ * @version [1.0, 2018/5/7 17:52]
+ */
+public interface Tree {
+
+	Node find(int key);
+
+	void insert(int key, double data);
+
+	void delete(int key);
+}
