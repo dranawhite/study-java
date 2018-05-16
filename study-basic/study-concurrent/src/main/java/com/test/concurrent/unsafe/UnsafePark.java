b63cea8615d7d036f580a660a3ac8317102e155b
dranawhite <1458755325@qq.com>
2018-05-16 11:40:09 +0800

    线程挂起与恢复

---
+++ study-basic/study-concurrent/src/main/java/com/test/concurrent/unsafe/UnsafePark.java
@@ -0,0 +1,33 @@
+package com.test.concurrent.unsafe;
+
+import com.dranawhite.exception.DranawhiteException;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * 线程挂起与恢复
+ *
+ * @author liangyq
+ * @version [1.0, 2018/5/16 11:33]
+ */
+public class UnsafePark extends Thread {
+
+	@Override
+	public void run() {
+		System.out.println("This is a Thread!");
+		LockSupport.park();
+		System.out.println("I am back!");
+	}
+
+	public static void main(String[] args) {
+		try {
+			UnsafePark park = new UnsafePark();
+			park.start();
+			TimeUnit.SECONDS.sleep(5);
+			LockSupport.unpark(park);
+		} catch (InterruptedException ex) {
+			throw new DranawhiteException();
+		}
+	}
+}
