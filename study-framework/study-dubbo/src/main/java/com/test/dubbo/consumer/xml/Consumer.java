d1ff7881e02a2d2c5675fc66cff7b6eb7101d804
dranawhite <1458755325@qq.com>
2018-05-17 14:51:57 +0800

    Dubbo ZooKeeper操作

--- study-framework/study-dubbo/src/main/java/com/test/dubbo/consumer/xml/Consumer.java
+++ study-framework/study-dubbo/src/main/java/com/test/dubbo/consumer/xml/Consumer.java
@@ -1,8 +1,11 @@
 package com.test.dubbo.consumer.xml;
 
+import com.dranawhite.exception.DranawhiteException;
 import com.test.dubbo.provider.DubboRequest;
 import com.test.dubbo.provider.IDubboService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
 
 /**
  * @author liangyq
@@ -11,14 +14,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Consumer {
 
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context =
-				new ClassPathXmlApplicationContext("applicationContext-dubbo-consumer.xml");
-		context.start();
-		IDubboService dubboService = context.getBean("dubboService", IDubboService.class);
-		DubboRequest request = new DubboRequest();
-		request.setId(19);
-		request.setName("jerry");
-		String hello = dubboService.service(request).getData();
-		System.out.println(hello);
+		try {
+			ClassPathXmlApplicationContext context =
+					new ClassPathXmlApplicationContext("applicationContext-dubbo-consumer.xml");
+			context.start();
+			IDubboService dubboService = context.getBean("dubboService", IDubboService.class);
+			DubboRequest request = new DubboRequest();
+			request.setId(19);
+			request.setName("jerry");
+			String hello = dubboService.service(request).getData();
+			System.out.println(hello);
+			System.in.read();
+		} catch (IOException ex) {
+			throw new DranawhiteException(ex);
+		}
 	}
 }
