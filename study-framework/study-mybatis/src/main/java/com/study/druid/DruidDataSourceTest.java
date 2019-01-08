eec7ae53dc36effabb49181db71478edaccaf635
dranawhite <1458755325@qq.com>
2019-01-08 20:44:00 +0800

    测试try with resource finally语句块逻辑

--- study-framework/study-mybatis/src/main/java/com/study/druid/DruidDataSourceTest.java
+++ study-framework/study-mybatis/src/main/java/com/study/druid/DruidDataSourceTest.java
@@ -3,35 +3,38 @@
  */
 package com.study.druid;
 
-import com.alibaba.druid.pool.DruidDataSource;
-import com.alibaba.druid.pool.DruidPooledConnection;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- *
  * @author liangyuquan
  * @version $Id: DruidDataSourceTest.java, v 0.1 2019-01-03 14:51 liangyuquan Exp $$
  */
 public class DruidDataSourceTest {
 
-    public static void main(String[] args) throws SQLException {
-        DruidDataSource dataSource = new DruidDataSource();
-        dataSource.setUrl("jdbc:mysql://localhost:3306/test");
-        dataSource.setMaxActive(50);
-        dataSource.setInitialSize(10);
-        dataSource.setUsername("root");
-        dataSource.setPassword("root");
+//    public static void main(String[] args) throws SQLException {
+//        DruidDataSource dataSource = new DruidDataSource();
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/test");
+//        dataSource.setMaxActive(50);
+//        dataSource.setInitialSize(10);
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("root");
+//
+//        DruidPooledConnection conn = dataSource.getConnection();
+//        String sql = "select user_id, job_number, user_name from user_team";
+//        PreparedStatement ps = conn.prepareStatement(sql);
+//        ResultSet rs = ps.executeQuery();
+//        rs.next();
+//        System.out.println(rs.getObject(1) + ": " + rs.getObject(2) + ": " + rs.getObject(3));
+//        conn.close();
+//    }
 
-        DruidPooledConnection conn = dataSource.getConnection();
-        String sql = "select user_id, job_number, user_name from user_team";
-        PreparedStatement ps = conn.prepareStatement(sql);
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        System.out.println(rs.getObject(1) + ": " + rs.getObject(2) + ": " + rs.getObject(3));
-        conn.close();
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        String url = "jdbc:mysql://localhost:3306/test";
+        String username = "root";
+        String password = "root";
+
+        DriverManager.getConnection(url, username, password);
     }
 }
