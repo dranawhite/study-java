944527f5de80c11dae7dcfb268680be5a18cd85c
dranawhite <1458755325@qq.com>
2019-01-22 10:57:15 +0800

    UrlClassLoader加载器

--- study-basic/study-jvm/src/main/java/com/study/jvm/classloader/UrlClassloaderPro.java
+++ study-basic/study-jvm/src/main/java/com/study/jvm/classloader/UrlClassloaderPro.java
@@ -1,13 +1,27 @@
 package com.study.jvm.classloader;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+
 /**
+ * 用于从JAR或者目录中加载类或者资源
+ *
  * @author dranawhite
  * @version [1.0, 2018/4/28 15:32]
  */
 public class UrlClassloaderPro {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		String path = UrlClassloaderPro.class.getProtectionDomain().getCodeSource().getLocation().getFile();
+		URL[] urlArr = new URL[]{new URL("file", null, path)};
+		URLClassLoader classLoader = new URLClassLoader(urlArr, Thread.currentThread().getContextClassLoader());
+		Class clz = classLoader.loadClass("com.study.jvm.classloader.UrlClassloaderPro");
+		UrlClassloaderPro pro = (UrlClassloaderPro) clz.newInstance();
+		pro.print();
+	}
 
+	public void print() {
+		System.out.println("Hello ClassLoader!");
 	}
 
 }
