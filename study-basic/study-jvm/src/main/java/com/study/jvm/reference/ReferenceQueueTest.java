f048a647a28aed11884f738efd846c3a1d0e3849
dranawhite <dranawhite@aliyun.com>
2019-08-05 10:35:21 +0800

    WeakReference弱引用探究

--- study-basic/study-jvm/src/main/java/com/study/jvm/reference/ReferenceQueueTest.java
+++ study-basic/study-jvm/src/main/java/com/study/jvm/reference/ReferenceQueueTest.java
@@ -5,30 +5,39 @@ import java.lang.ref.ReferenceQueue;
 import java.lang.ref.WeakReference;
 
 /**
+ * ReferenceQueue保存已经被回收的WeakReference对象
+ * <pre>
+ * 运行结果：
+ *  Reference Queue is listening.
+ *  Ready to gc
+ *  java.lang.ref.WeakReference@dc7a4fc event fired.
+ *  wr.get: null
+ * </pre>
+ *
  * @author dranawhite 2017/8/4
  * @version 1.0
  */
 class ReferenceQueueTest {
 
     public static void main(String[] args) {
-        final ReferenceQueue q = new ReferenceQueue();
+        final ReferenceQueue<String> q = new ReferenceQueue<>();
+        // String对象才能被回收，常量字符串会被缓存到常量池中
         String str = new String("AK47");
-        WeakReference wr = new WeakReference(str, q);
-        Thread t = new Thread() {
-            @Override
-            public void run() {
-                try {
-                    Reference reference = q.remove();
-                    System.out.println(reference + " event fired.");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        WeakReference<String> wr = new WeakReference<>(str, q);
+        Thread t = new Thread(() -> {
+            try {
+                // 如果WeakReference对象被回收时才会获取到对象
+                Reference reference = q.remove();
+                System.out.println(reference + " event fired.");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        };
+        });
         t.setDaemon(true);
         t.start();
         System.out.println("Reference Queue is listening.");
-        str = null; // clear strong reference
+        // clear strong reference
+        str = null;
         System.out.println("Ready to gc");
         System.gc();
         try {
--- study-basic/study-jvm/src/main/java/com/study/jvm/reference/WeakMapPro.java
+++ study-basic/study-jvm/src/main/java/com/study/jvm/reference/WeakMapPro.java
@@ -3,9 +3,6 @@ package com.study.jvm.reference;
 import java.lang.ref.WeakReference;
 import java.util.WeakHashMap;
 
-import lombok.Getter;
-import lombok.Setter;
-
 /**
  * 弱引用实验
  * <pre>
@@ -14,17 +11,36 @@ import lombok.Setter;
  *     可以用WeakReference包裹值
  * </pre>
  *
+ * <pre>
+ *     WeakHashMap的实现：
+ *     1）Entry继承了WeakReference对象
+ *     2）Map的key被保存在WeakReference中，则每次key会失效
+ *     3）在每次查询，删除，插入的时候，会调用ReferenceQueue对被删除的Entry对象进行遍历删除
+ *
+ *     所以，如果map中的key被强引用的话，map中的该个key，并不会失效；
+ * </pre>
+ *
+ * <pre>
+ *     运行结果：
+ *     200
+ *     SIZE = 977438
+ *     SIZE = 128
+ *
+ *     Integer会对-128到127的对象进行缓存，既0-127的对象被缓存池引用，无法被回收。
+ * </pre>
+ *
  * @author dranawhite
  * @version $Id: WeakMapPro.java, v 0.1 2018-09-12 19:16 dranawhite Exp $$
  */
 public class WeakMapPro {
 
     public static void main(String[] args) throws InterruptedException {
-        WeakHashMap<Integer, WeakReference<Integer>> map = new WeakHashMap();
-        for (int i = 0; i < 1024 * 1024; i++) {
-            Integer num = new Integer(i + 128);
-            WeakReference<Integer> reference = new WeakReference<>(num);
-            map.put(num, reference);
+        final int size = 1024 * 1024;
+        WeakHashMap<Integer, WeakReference<Integer>> map = new WeakHashMap<>();
+        for (int i = 0; i < size; i++) {
+            // 创建一个不被Integer对象自动缓存的integer对象
+            WeakReference<Integer> reference = new WeakReference<>(i);
+            map.put(i, reference);
         }
         System.out.println(map.get(200).get());
         System.out.println("SIZE = " + map.size());
@@ -32,20 +48,4 @@ public class WeakMapPro {
         Thread.sleep(5000);
         System.out.println("SIZE = " + map.size());
     }
-
 }
-
-@Setter
-@Getter
-class PersonPO {
-
-    Integer id;
-
-    PersonPO(Integer id) {
-        this.id = id;
-    }
-
-    PersonPO() {}
-
-}
-
