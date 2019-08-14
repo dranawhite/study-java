218125ee907828ee7b32d5dfbe8abae9c8dc10e2
dranawhite <dranawhite@aliyun.com>
2019-08-14 14:56:37 +0800

    访问者模式

--- study-pattern/src/main/java/com/dranawhite/study/pattern/visitor/Element.java
+++ study-pattern/src/main/java/com/dranawhite/study/pattern/visitor/Element.java
@@ -1,6 +1,7 @@
 package com.dranawhite.study.pattern.visitor;
 
 /**
+ * 声明接受哪一类访问者访问
  *
  * @author dranawhite
  * @version $Id: Element.java, v 0.1 2019-01-02 17:12 dranawhite Exp $$
--- study-pattern/src/main/java/com/dranawhite/study/pattern/visitor/Main.java
+++ study-pattern/src/main/java/com/dranawhite/study/pattern/visitor/Main.java
@@ -2,6 +2,9 @@ package com.dranawhite.study.pattern.visitor;
 
 /**
  * 访问者模式
+ * <pre>
+ *     是对迭代器模式的扩充，可以充当拦截器的角色
+ * </pre>
  *
  * @author dranawhite
  * @version $Id: Main.java, v 0.1 2019-01-02 17:24 dranawhite Exp $$
