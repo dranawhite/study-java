edb3160c0b005ffb88d0792e14f4a660cf38a187
dranawhite <dranawhite@aliyun.com>
2019-08-29 10:04:10 +0800

    Spring Starter探究

---
+++ study-framework/study-springboot/src/main/java/com/dranawhite/study/springboot/web/StarterController.java
@@ -0,0 +1,33 @@
+package com.dranawhite.study.springboot.web;
+
+import com.dranawhite.api.model.DranaResponse;
+import com.dranawhite.starter.HelloService;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Spring Starter
+ *
+ * @author dranawhite
+ * @version : StarterController.java, v 0.1 2019-08-29 9:44 dranawhite Exp $$
+ */
+@RestController
+@RequestMapping("/starter")
+@Slf4j
+public class StarterController {
+
+    @Autowired
+    private HelloService helloService;
+
+    @GetMapping("/say")
+    public DranaResponse<Boolean> sayHello() {
+        log.info("Say Hello!");
+        log.info(helloService.sayHello());
+        return DranaResponse.success(Boolean.TRUE);
+    }
+}
